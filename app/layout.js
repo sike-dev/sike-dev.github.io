d2cd2f238316ed37e070b1101da43fb14e66aa66
jesi318 <jeswin.p.jaison@gmail.com>
2023-09-02 11:06:14 +0000

    added loading.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
       <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
     </head>
-    <body>{children}</body>
+    <body>
+      <Suspense fallback={<Loading />}>{children}</Suspense>
+    </body>
   </html>
 );
 
---
+++ app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="loading bg-black w-screen h-screen items-center justify-center">
+      <img src="./sike_1.png" alt="loading" />
+      <h1 className="text-white">loading ...</h1>
+    </div>
+  );
+}
