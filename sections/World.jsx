d1cd3314f0b3c86cf9f2ed40cfa63eccbd47f258
jesi318 <jeswin.p.jaison@gmail.com>
2023-09-02 08:34:25 +0000

    changed map images to absolute path-re

--- sections/World.jsx
+++ sections/World.jsx
@@ -25,7 +25,7 @@ const World = () => (
         variants={fadeIn("up", "tween", 0.3, 1)}
         className="relative mt-[68px] flex w-full h-[550px]"
       >
-        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+        <img src="map.png" alt="map" className="w-full h-full object-cover" />
 
         <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
