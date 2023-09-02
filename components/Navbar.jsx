9b1c0ce0ef38431679eea7f67885368fb329bcfd
jesi318 <jeswin.p.jaison@gmail.com>
2023-09-02 08:14:46 +0000

    changed immage to absolute path

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { motion } from "framer-motion";
 import styles from "../styles";
 import { navVariants } from "@/utils/motion";
@@ -13,15 +14,15 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
-        src="/search.svg"
+        src="./search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
 
-      <img src="/sike_1.png" alt="sike_1" className="h-[40px] w-[120px]" />
+      <img src="./sike_1.png" alt="sike_1" className="h-[40px] w-[120px]" />
 
       <img
-        src="/menu.svg"
+        src="./menu.svg"
         alt="menu"
         className="w-[24px] h-[24px] object-contain"
       />
